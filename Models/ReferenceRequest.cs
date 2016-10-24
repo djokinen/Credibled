028f635ecd36a5390fdd48ea59df1c0527c1ea68
djokinen <djokinen@yahoo.com>
2016-10-24 13:12:43 -0400

    Datamodel updates

--- Models/ReferenceRequest.cs
+++ Models/ReferenceRequest.cs
@@ -15,31 +15,40 @@ namespace Credibled.Models
 		public Boolean Enabled { get; set; }
 
 		[Required]
+		[Editable(false, AllowInitialValue = true)]
 		[DataType(DataType.DateTime)]
+		[Display(GroupName = "Candidate", Description = "Created date", Name = "Created", Prompt = "Created date", ShortName = "Created")]
 		public DateTime CreatedDate { get; set; }
 
 		[Required]
+		[Editable(false, AllowInitialValue = true)]
+		[Display(GroupName = "Candidate", Description = "Last modified date", Name = "Modified", Prompt = "Last modified date", ShortName = "Modified")]
 		[DataType(DataType.DateTime)]
 		public DateTime LastModifiedDate { get; set; }
 
 		#endregion
 
 		[Required]
+		[Display(GroupName = "Candidate", Description = "Candidate Name", Name = "Candidate", Prompt = "Candidate name", ShortName = "Candidate")]
 		public String CandidateId { get; set; }
 
 		[Required]
-		public string RefereeId { get; set; }
-
-		[Required]
 		[DataType(DataType.Date)]
+		[Display(GroupName = "Candidate", Description = "Candidate start date", Name = "Start", Prompt = "Start Date", ShortName = "Start")]
 		public DateTime CandidateStartDate { get; set; }
 
 		[Required]
 		[DataType(DataType.Date)]
+		[Display(GroupName = "Candidate", Description = "Candidate end date", Name = "End", Prompt = "End Date", ShortName = "End")]
 		public DateTime CandidateEndDate { get; set; }
 
 		[Required]
 		[DataType(DataType.MultilineText)]
+		[Display(GroupName = "Candidate", Description = "Candidate job duties", Name = "Duties", Prompt = "Candidate job duties", ShortName = "Duties")]
 		public String CandidateJobDuties { get; set; }
+
+		[Required]
+		[Display(GroupName = "Referee", Description = "Referee Name", Name = "Referee", Prompt = "Referee name", ShortName = "Referee")]
+		public string RefereeId { get; set; }
 	}
 }
